85186fa7f7ce5fa670f8a290b0db6a20c4eeed6c
yakovlevav <im.botan@gmail.com>
2018-09-24 17:43:54 +0200

    Folder initialization file added'

---
+++ initialize.py
@@ -0,0 +1,26 @@
+import	os
+
+DataFolderName = 'Data'
+DataFolders = ['PalmSense', 'Humidity', 'Oxygen']
+
+ResultFolder = 'Result'
+ResultFolders = ['Response', 'RawData', 'Oxygen']
+
+
+def make_folders(i):
+    '''Makes a folder (and its parents) if not present'''
+    try:  
+        os.makedirs(i)
+        print('Folder '+i)
+    except:
+        pass
+
+if __name__ == '__main__':
+	make_folders(DataFolderName)
+	#Create Subfolders in Data
+	for i in DataFolders: 
+		make_folders(DataFolderName+'/'+i)
+	#Create result folder
+	make_folders(ResultFolder)
+
+
