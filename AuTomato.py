8b369a8879115f0cca4afa2880b4ac2184b4a09b
yakovlevav <im.botan@gmail.com>
2018-09-24 20:20:36 +0200

    initialization function added

--- AuTomato.py
+++ AuTomato.py
@@ -1,8 +1,15 @@
 import AutoChrono
+import initialize
+
 if __name__ == '__main__':
+
+	init = initialize.WoldCreation()
+	init.create_folders()
+	init.clean_folders()
+
 	# AutoChrono.CSVConverter()
-	dc = AutoChrono.DataCollector()
-	dc.GetOxygenFromBoard()
+	# dc = AutoChrono.DataCollector()
+	# dc.GetOxygenFromBoard()
 	# dc.getcurrent(2)
 	# dc.getoxygen()
 	# dc.gethumidity()
