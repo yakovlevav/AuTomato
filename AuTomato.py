de53bbd550ca483ea334edbd67b458d1fc856f25
yakovlevav <im.botan@gmail.com>
2018-09-24 17:26:35 +0200

    Statr file added
    wq

---
+++ AuTomato.py
@@ -0,0 +1,12 @@
+import AutoChrono
+if __name__ == '__main__':
+	# AutoChrono.CSVConverter()
+	dc = AutoChrono.DataCollector()
+	dc.GetOxygenFromBoard()
+	# dc.getcurrent(2)
+	# dc.getoxygen()
+	# dc.gethumidity()
+	# dc.writedata()
+
+	# pt = AutoChrono.Plotter()
+	# pt.plotdata()
