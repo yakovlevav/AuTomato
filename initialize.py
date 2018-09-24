6426938b547e295df30ea9ef9ccde930a535856d
yakovlevav <im.botan@gmail.com>
2018-09-24 20:21:16 +0200

    Initialization class created. Now you can create and clean up folders

--- initialize.py
+++ initialize.py
@@ -1,30 +1,53 @@
 import	os
+from glob import glob
 
-FunctionsFolder = 'Functions'
+class WoldCreation:
+	"""docstring for Itinialise"""
 
-DataFolderName = 'Data'
-DataFolders = ['PalmSense', 'Humidity', 'Oxygen']
+	Folders = {
+		'Data': ['PalmSense', 'Humidity', 'Oxygen', 'OxygenBoard'],
+		'Result': ['Response', 'RawData', 'Oxygen']
+		}
 
-ResultFolder = 'Result'
-ResultFolders = ['Response', 'RawData', 'Oxygen']
+	# def __init__(self, arg):
+	# 	super(Itinialise, self).__init__()
+	# 	self.arg = arg
+
+	def maker(self, i):
+	    '''Makes a folder (and its parents) if not present'''
+	    try:  
+	        os.makedirs(i)
+	    except FileExistsError as e:
+	    	print(e, '\n Folder' + i +' already exists! \n')
+	    except Exception as e:
+	    	print(e, 'Sorry. Something went wrong!')
+	    else:
+	    	print('Folder '+i+' are created...')
 
 
-def make_folders(i):
-    '''Makes a folder (and its parents) if not present'''
-    try:  
-        os.makedirs(i)
-        print('Folder '+i)
-    except:
-        pass
+	def cleaner(self,i):
+		for x in glob(i+'/*'):
+			try:
+				os.remove(x)
+			except FileNotFoundError as e:
+				print(e, "\n Sorry, i can't clean files! Probably folder already empty!\n")
+			except PermissionError as e:
+				print(e, "\n Sorry, i can't clean files! Probably file in use, or opened!\n")
+			except Exception as e: 
+				print(e)
+			else:
+				print('File:' + x + 'removed!')
 
-if __name__ == '__main__':
-	make_folders(DataFolderName)
-	#Create Subfolders in Data
-	for i in DataFolders: 
-		make_folders(DataFolderName+'/'+i)
-	#Create result folder
-	make_folders(ResultFolder)
-	make_folders(FunctionsFolder)
+	def create_folders(self):
+		#Create Subfolders in Data
+		for Lvl1, Lvl2  in self.Folders.items(): 
+			self.maker(Lvl1)
+			for x in Lvl2:
+				self.maker(Lvl1+'/'+x)
 
+	def clean_folders(self):
+		for Lvl1, Lvl2  in self.Folders.items(): 
+			for x in Lvl2:
+				self.cleaner(Lvl1+'/'+x)
 
 
