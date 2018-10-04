d7df7547d8257d758709b986a120ab3b0e7668f4
yakovlevav <im.botan@gmail.com>
2018-10-04 21:26:42 +0200

    Separate convertor for PalmSense from file

--- AuTomato.py
+++ AuTomato.py
@@ -1,24 +1,40 @@
 import AutoChrono
 import initialize
+from converters import ConvertPalmSenseCSV
+# import logging
 
-def initclean():
-	init = initialize.initialize()
+# logging.basicConfig(level = logging.WARNING)
+
+# def initclean():
+	# init = initialize.initialize()
 	# init.create_folders()
 	# init.clean_all_folders()
 	# init.clean_data_folders()
 	# init.clean_specific('Oxygen')
 
 if __name__ == '__main__':
-	
-	initclean()
+	ConvertPalmSenseCSV()
 
-	# AutoChrono.CSVConverter()
+	
+	# initclean()
+	# cv = CSVConverter()
+
+
+	# # AutoChrono.CSVConverter()
 	# dc = AutoChrono.DataCollector()
-	# dc.GetOxygenFromBoard()
-	# dc.getcurrent(2)
-	# dc.getoxygen()
-	# dc.gethumidity()
+	# # dc.OxygenBoardData()
+	# # dc.getoxygen()
+
+	# dc.getoxygenboard()
+	# # print(dc.Data)
+	# # dc.GetOxygenFromBoard()
+
+	# # dc.getcurrent(2)
+	# # dc.getoxygen()
+	# # dc.gethumidity()
 	# dc.writedata()
 
 	# pt = AutoChrono.Plotter()
+	# pt.plotnames = ['Oxygen lvl', 'Current(nA)', 'Time']
+	# pt.ShowTime = False
 	# pt.plotdata()
