a10e77b305bb38796343f9f61749c42fe52936cc
yakovlevav <im.botan@gmail.com>
2018-10-05 14:00:21 +0200

    Tool set file for use in all modules

---
+++ tools.py
@@ -0,0 +1,15 @@
+from glob import glob
+
+def getfilelist(path, filetype, comment = 'No comment'):
+	'''
+	Get list of files in path and check if files exist
+	'''
+	filefils = sorted(glob(path + filetype))
+	if not filefils: 
+		print(comment)
+		print('No data in path: %s'%path)
+		sys.exit('Programm crashed!')
+	else: 
+		print('Collect data file for path %s'%(path+filetype))
+		print('Number of files: {}'.format(len(filefils)))
+		return filefils
