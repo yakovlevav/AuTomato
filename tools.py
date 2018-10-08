7d9069b39f1aaf5a28a2de1e0ba4d59b6cc58f01
yakovlevav <im.botan@gmail.com>
2018-10-08 18:24:00 +0200

    Added function of get file name from path and extention

--- tools.py
+++ tools.py
@@ -1,17 +1,21 @@
 from glob import glob
-import sys
+import sys, os
 import settings as st
 
 def getfilelist(path, filetype, comment = 'No comment'):
 	'''
 	Get list of files in path and check if files exist
 	'''
-	filefils = sorted(glob(path + filetype))
+
+	filefils = sorted(glob(path + '*'+ filetype))
+	print(comment)
 	if not filefils: 
-		print(comment)
-		print('No data in path: %s'%path)
+		print('No data in path: %s with extention %s'%(path,filetype))
 		sys.exit('Programm crashed!')
 	else: 
 		print('Collect data file for path %s'%(path+filetype))
 		print('Number of files: {}'.format(len(filefils)))
 		return filefils
+
+def FindFilename(path):
+	return os.path.splitext(os.path.basename(path))[0]
